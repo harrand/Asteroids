d6a93e505a4234f8f8c9bae2901181dc9f00ef02
Harrand <harrysjh98@gmail.com>
2018-05-22 23:02:11 +0100

    Added ingame escape-menu

---
+++ Assets/Scripts/VisibilityToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityToggle : MonoBehaviour
+{
+    GameObject canvas_object;
+
+    void Start()
+    {
+        this.canvas_object = GameObject.FindGameObjectWithTag("Escape Menu");
+        this.SetInvisible();
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            this.ToggleVisible();
+    }
+
+    public void SetVisible()
+    {
+        this.canvas_object.SetActive(true);
+    }
+
+    public void SetInvisible()
+    {
+        this.canvas_object.SetActive(false);
+    }
+
+    public void ToggleVisible()
+    {
+        this.canvas_object.SetActive(!this.canvas_object.activeSelf);
+    }
+}
